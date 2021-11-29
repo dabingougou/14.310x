f095dea8d199a825d8cc6e0dae19a60c3a53edb4
dabingougou <dabingougou@gmail.com>
2021-11-30 00:53:24 +0800

    Finished hw 8

--- module8/R_code/hw8.R
+++ module8/R_code/hw8.R
@@ -14,5 +14,26 @@ names(my_fit)
 plot(my_cars$mpg, my_cars$wt)
 abline(my_fit, col="red")
 
-# read.csv("/module8/R_code/data/nlsw88.csv")
-# setwd("./module8/R_code")
+data <- read.csv("data/nlsw88.csv")
+glimpse(data)
+
+my_model <- lm(data=data, formula=lwage~yrs_school)
+summary(my_model)
+
+coefficients(my_model)
+ci <- confint(my_model, level=0.9)
+
+sum(residuals(my_model))
+
+upper <- 0.092920 + qnorm(0.95, mean=0, sd=1) * 0.004333
+lower <- 0.092920 - qnorm(0.95, mean=0, sd=1) * 0.004333
+
+Mincer_ml <- lm(data=data, formula=lwage ~ yrs_school + ttl_exp)
+summary(Mincer)
+summary(Mincer_ml)
+
+wwdata <- mutate(data, se = yrs_school + 2 * ttl_exp)
+glimpse(wwdata)
+restricted_Mincer_ml <- lm(data=wwdata, formula=lwage ~ se)
+summarize(restricted_Mincer_ml)
+summary(restricted_Mincer_ml)
